7cd6292fad54f865d3290e49a0e0c9f7c3dc0645
Demiladeala <demiladeala@gmail.com>
2024-02-05 07:21:18 +0100

    first commit

---
+++ BasicCalculator/Domain/CalculatorManagement/Calculate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Domain.CalculatorManagement
+{
+    internal class Calculate
+    {
+         double inputOne = 0;
+         double inputTwo = 0;
+         double result;
+
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+        public double SquareRoot(double a)
+        {
+            return Math.Sqrt(a);
+        }
+        public double CubeRoot(double b)
+        {
+            return Math.Cbrt(b);
+        }
+    }
+}
---
+++ BasicCalculator/Utilities.cs
@@ -0,0 +1,159 @@
+﻿using BasicCalculator.Domain.CalculatorManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    internal class Utilities
+    {
+        internal static void ProgramMenu()
+        {
+            Console.WriteLine("Welcome! This is a BASIC CALCULATOR");
+            string? userSelection;
+            do
+            {
+
+                Console.WriteLine("Select the Operation you want to perform");
+                Console.WriteLine("1: Addition");
+                Console.WriteLine("2: Subtraction");
+                Console.WriteLine("3: Multiplication");
+                Console.WriteLine("4: Division");
+                Console.WriteLine("5: Square Root");
+                Console.WriteLine("6: Cube Root \n");
+                Console.WriteLine("0: Close Application");
+
+                Console.Write("Your Selection: ");
+                userSelection = Console.ReadLine();
+
+                switch (userSelection)
+                {
+                    case "1":
+                        AdditionCalculation();
+                        break;
+                    case "2":
+                        SubtractionCalculation();
+                        break;
+                    case "3":
+                        MultiplicationCalculation();
+                        break;
+                    case "4":
+                        DivisionCalculation();
+                        break;
+                    case "5":
+                        SquareRootCalculation();
+                        break;
+                    case "6":
+                        CubeRootCalculation();
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection. Please try again.");
+                        break;
+                }
+            } 
+            while(userSelection != "0");
+            
+        }
+
+        public static void AdditionCalculation()
+        {
+            double inputOne = 0;
+            double inputTwo = 0;
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            inputOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.Addition(inputOne, inputTwo);
+
+            Console.WriteLine($"Your result: {inputOne} + {inputTwo} = {result}");
+        }
+
+        public static void SubtractionCalculation()
+        {
+            double inputOne = 0;
+            double inputTwo = 0;
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            inputOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.Subtraction(inputOne, inputTwo);
+
+            Console.WriteLine($"Your result: {inputOne} - {inputTwo} = {result}");
+        }
+
+        public static void MultiplicationCalculation()
+        {
+            double inputOne = 0;
+            double inputTwo = 0;
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            inputOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.Multiplication(inputOne, inputTwo);
+
+            Console.WriteLine($"Your result: {inputOne} x {inputTwo} = {result}");
+        }
+
+        public static void DivisionCalculation()
+        {
+            double inputOne = 0;
+            double inputTwo = 0;
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            inputOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.Division(inputOne, inputTwo);
+
+            Console.WriteLine($"Your result: {inputOne} / {inputTwo} = {result}");
+        }
+
+        public static void SquareRootCalculation()
+        {
+            double inputOne = 0;
+
+            double result;
+
+            Console.WriteLine("Enter number: ");
+            inputOne = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.SquareRoot(inputOne);
+
+            Console.WriteLine($"Your result: Sqrt of {inputOne} = {result}");
+        }
+
+        public static void CubeRootCalculation()
+        {
+            double inputTwo = 0;
+            double result;
+
+            Console.WriteLine("Enter number: ");
+            inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            Calculate addition = new Calculate();
+            result = addition.CubeRoot(inputTwo);
+
+            Console.WriteLine($"Your result: Sqrt of {inputTwo} = {result}");
+        }
+    }
+}
